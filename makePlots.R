704f547581e4550a6e6a864e53e8f06dc9931bab
Dirk Hesse <dirk.hesse@intelcom.no>
2016-04-13 12:05:07 +0200

    Adding code for the URL post.

--- makePlots.R
+++ makePlots.R
@@ -5,3 +5,13 @@ ggplot(topTags, aes(x=Hashtag, y=`Use Count`)) +
   geom_bar(stat="identity", fill="steelblue") +
   theme(axis.text.x = element_text(angle = 90, hjust=1, vjust=0.5))
 ggsave("topTags.png")
+topURLs <- read_csv("topURLs.csv")
+ggplot(topURLs, aes(x=`Base URL`, y=`Use Count`)) +
+  geom_bar(stat="identity", fill="steelblue") +
+  theme(axis.text.x = element_text(angle = 90, hjust=1, vjust=0.5))
+ggsave("topURLs.png")
+topURLsExpanded <- read_csv("topURLsExpanded.csv")
+ggplot(topURLsExpanded, aes(x=`Base URL`, y=`Use Count`)) +
+  geom_bar(stat="identity", fill="steelblue") +
+  theme(axis.text.x = element_text(angle = 90, hjust=1, vjust=0.5))
+ggsave("topURLsExpanded.png")
